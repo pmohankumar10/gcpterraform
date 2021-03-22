26b3f6b867e61bc569da175e2140c2209a41f48e
pmohankumar <69764145+pmohankumar10@users.noreply.github.com>
2021-03-22 13:13:17 +0530

    cursor update

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
-provider “google” {
+provider "google" {
 #credentials = file(“/root/secret/hostbread-d44243ebddf5.json”)
-project = “Learn project”
-region = “us-central1”
-zone = “us-central1-c”
+project = "Learn project"
+region = "us-central1"
+zone = "us-central1-c"
 }
 
-resource “google_compute_instance” “vm_instance” {
-name = “new-tf-vm”
-machine_type = “f1-micro”
+resource "google_compute_instance" "vm_instance" {
+name = "new-tf-vm"
+machine_type = "f1-micro"
 
 boot_disk {
 initialize_params {
-image = “debian-cloud/debian-9”
+image = "debian-cloud/debian-9"
 }
 }
 
