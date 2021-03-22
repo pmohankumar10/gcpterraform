e5f184ed367dd6d19e38e2dab11aabf431d91782
pmohankumar <69764145+pmohankumar10@users.noreply.github.com>
2021-03-22 14:56:57 +0530

    zone rollbacked

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "google" {
 #credentials = file(“/root/secret/hostbread-d44243ebddf5.json”)
 project = "Learn project"
 #region = "us-central1"
-zone = "us-central1-b"
+zone = "us-central1-a"
 }
 
 resource "google_compute_instance" "vm_instance" {
